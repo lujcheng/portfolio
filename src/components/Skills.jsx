35d75e69497e0ace0d5337e17d6c06a1e69f6c6a
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-03 19:02:29 -0400

    added svgs to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,22 +6,35 @@ class Skills extends Component {
       <span> 
         <div>
           <h3>Web Front End</h3>
-          <i class="fab fa-react">React</i>
+          <img src='../../public/react.svg' alt='react' />
+          <img src='../../public/jquery.svg' alt='jquery' />
+          <img src='../../public/sass.svg' alt='sass' /> 
         </div>
         <div>
           <h3>Web Back End</h3>
+          <img src='../../public/node.svg' alt='node' />
+          <img src='../../public/rails.svg' alt='rails' />
+          <img src='../../public/express.svg' alt='express' />
+          <img src='../../public/socket-io.svg' alt='socket-io' />
+          <img src='../../public/docker.svg' alt='docker' /> 
+          <img src='../../public/mocha.svg' alt='mocha' />
+          <img src='../../public/babel.svg' alt='babel' /> 
         </div>
         <div>
           <h3>Databases</h3>
+          <img src='../../public/psql.svg' alt='psql' />
+          <img src='../../public/mongo.svg' alt='mongodb' />
         </div>
         <div>
           <h3>Languages</h3>
-          <i class="fab fa-js"></i>
-          <i class="fab fa-html5"></i>
-          <i class="devicon-css3-plain-wordmark colored">CSS</i>
+          <img src='../../public/js.svg' alt='javascript' />
+          <img src='../../public/html.svg' alt= 'html5' />
+          <img src='../../public/css.svg' alt='CSS' />
         </div>
         <div>
           <h3>Source Control Management</h3>
+          <img src='../../public/git.svg' alt='git' />
+          <img src='../../public/github.svg' alt='github' />
         </div>
       </span>
     )
