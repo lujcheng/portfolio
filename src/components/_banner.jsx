7958964274087c875be6b112ac781b4d75a047f1
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-03 15:51:34 -0400

    added phrase to banner

--- src/components/_banner.jsx
+++ src/components/_banner.jsx
@@ -5,6 +5,9 @@ class Banner extends Component {
     return (
       <span> 
         <img src='./ebrastij.svg' width='500px' height='500px' />
+        <span>
+          I am a: Developer
+        </span>
       </span>
     )
   }
