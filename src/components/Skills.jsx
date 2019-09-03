f21d1c5a33a72927521b291344654638beca2da0
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-03 19:32:59 -0400

    relocated and changed svgs to components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,40 +1,41 @@
 import React, { Component } from 'react'
-
+import Hml from '../icons/html'
 class Skills extends Component {
   render () {
     return (
       <span> 
         <div>
           <h3>Web Front End</h3>
-          <img src='../../public/react.svg' alt='react' />
-          <img src='../../public/jquery.svg' alt='jquery' />
-          <img src='../../public/sass.svg' alt='sass' /> 
+          <img src='./react.svg' alt='react' />
+          <img src='./jquery.svg' alt='jquery' />
+          <img src='./sass.svg' alt='sass' /> 
         </div>
         <div>
           <h3>Web Back End</h3>
-          <img src='../../public/node.svg' alt='node' />
-          <img src='../../public/rails.svg' alt='rails' />
-          <img src='../../public/express.svg' alt='express' />
-          <img src='../../public/socket-io.svg' alt='socket-io' />
-          <img src='../../public/docker.svg' alt='docker' /> 
-          <img src='../../public/mocha.svg' alt='mocha' />
-          <img src='../../public/babel.svg' alt='babel' /> 
+          <img src='./node.svg' alt='node' />
+          <img src='./rails.svg' alt='rails' />
+          <img src='./express.svg' alt='express' />
+          <img src='./socket-io.svg' alt='socket-io' className='icon' />
+          <img src='./docker.svg' alt='docker' /> 
+          <img src='./mocha.svg' alt='mocha' />
+          <img src='./babel.svg' alt='babel' /> 
         </div>
         <div>
           <h3>Databases</h3>
-          <img src='../../public/psql.svg' alt='psql' />
-          <img src='../../public/mongo.svg' alt='mongodb' />
+          <img src='./psql.svg' alt='psql' />
+          <img src='./mongo.svg' alt='mongodb' />
         </div>
         <div>
           <h3>Languages</h3>
-          <img src='../../public/js.svg' alt='javascript' />
-          <img src='../../public/html.svg' alt= 'html5' />
-          <img src='../../public/css.svg' alt='CSS' />
+          <img src='./js.svg' alt='javascript' />
+          <img src='./html.svg' alt= 'html5' />
+          <Hml className='icon'/>
+          <img src='./css.svg' alt='CSS' />
         </div>
         <div>
           <h3>Source Control Management</h3>
-          <img src='../../public/git.svg' alt='git' />
-          <img src='../../public/github.svg' alt='github' />
+          <img src='./git.svg' alt='git' />
+          <img src='./github.svg' alt='github' />
         </div>
       </span>
     )
