9a1e44e5d461dea373f464920bc30f79e5787a80
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-03 17:18:49 -0400

    added skills page

---
+++ src/components/Skills.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  render () {
+    return (
+      <span> 
+        <p>
+          My story starts in another industry, helping people heal their bodies.
+        </p>
+        <img src='' alt='rehab animation' />
+        <p>
+          I felt something was missing in my career and searched for something different.
+        </p>
+        <img src='' alt='black hole?' />
+        <p>
+          more...
+        </p>
+      </span>
+    )
+  }
+}
+
+export default About
