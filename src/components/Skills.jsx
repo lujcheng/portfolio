6737b27c0c106dc966b98fa7bdd7b072134365f9
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-03 19:50:44 -0400

    imported svgs to html

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,36 +6,35 @@ class Skills extends Component {
       <span> 
         <div>
           <h3>Web Front End</h3>
-          <img src='../../public/react-original.svg' alt='react' />
-          <img src='../../public/jquery-original.svg' alt='jquery' />
-          <img src='../../public/sass-original.svg' alt='sass' /> 
+          <img src='./react-original.svg' className='icon' alt='react' />
+          <img src='./jquery-original.svg' className='icon' alt='jquery' />
+          <img src='./sass-original.svg' className='icon' alt='sass' /> 
         </div>
         <div>
           <h3>Web Back End</h3>
-          <img src='../../public/nodejs-original.svg' alt='node' />
-          <img src='../../public/rails-original-wordmark.svg' alt='rails' />
-          <img src='../../public/express-original.svg' alt='express' />
-          <img src='./socket-io.svg' alt='socket-io' className='icon' />
-          <img src='./docker.svg' alt='docker' /> 
-          <img src='./mocha.svg' alt='mocha' />
-          <img src='./babel-original.svg' alt='babel' className='icon'/> 
+          <img src='./nodejs-original.svg' className='icon' alt='node' />
+          <img src='./rails-original-wordmark.svg' className='icon' alt='rails' />
+          <img src='./express-original.svg' className='icon' alt='express' />
+          <img src='./socket-io.svg' className='icon' alt='socket-io'  />
+          <img src='./docker-original.svg' className='icon' alt='docker' /> 
+          <img src='./mocha-plain.svg' className='icon' alt='mocha' />
+          <img src='./babel-original.svg' className='icon' alt='babel' /> 
         </div>
         <div>
           <h3>Databases</h3>
-          <img src='./psql.svg' alt='psql' />
-          <img src='./mongo.svg' alt='mongodb' />
+          <img src='./postgresql-original.svg' className='icon' alt='psql' />
+          <img src='./mongodb-original.svg' className='icon' alt='mongodb' />
         </div>
         <div>
           <h3>Languages</h3>
-          <img src='./js.svg' alt='javascript' />
-          <img src='./html.svg' alt= 'html5' />
-          <Hml />
-          <img src='./css.svg' alt='CSS' />
+          <img src='./javascript-original.svg' className='icon' alt='javascript' />
+          <img src='./html5-original.svg' className='icon' alt= 'html5' />
+          <img src='./css.svg' className='icon' alt='CSS' />
         </div>
         <div>
           <h3>Source Control Management</h3>
-          <img src='./git.svg' alt='git' />
-          <img src='./github.svg' alt='github' />
+          <img src='./git-original.svg' className='icon' alt='git' />
+          <img src='./github-original.svg' className='icon' alt='github' />
         </div>
       </span>
     )
