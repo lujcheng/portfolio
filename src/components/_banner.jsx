27c29ccd3bbf013a64e8dbfb73e16fd0dc34f75d
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-03 23:13:27 -0400

    added react typist

--- src/components/_banner.jsx
+++ src/components/_banner.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Typist from 'react-typist'
+import TypistLoop from 'react-typist-loop'
 
 class Banner extends Component {
   render () {
@@ -7,7 +9,14 @@ class Banner extends Component {
 
         <img src='./ebrastij.svg' width='500px' height='500px' />
         <span>
-          I am a: Developer
+          I am a: 
+          <TypistLoop interval={3000}>
+            {[
+              'Problem Solver',
+              'Developer',
+              'Good Person',
+            ].map(text => <Typist key={text} startDelay={1000}>{text}</Typist>)}
+          </TypistLoop>
         </span>
       </span>
     )
