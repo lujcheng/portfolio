816a719b7deece4e8eea9be8c07231acfba6b262
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-03 20:38:36 -0400

    created projects page

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+class Skills extends Component {
+  render () {
+    return (
+      <div>
+        these are my projects
+      </div>
+    )
+  }
+}
+
+export default Skills
