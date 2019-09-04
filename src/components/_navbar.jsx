28772831c1b8ddd74731bd06f7d9f9a6af40fe19
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-03 20:02:33 -0400

    added navbar

---
+++ src/components/_navbar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render () {
+    return (
+      <div>
+        <span>
+          <p>
+            Projects
+          </p>
+        </span>
+        <span>
+          <p>
+            Skills
+          </p>
+        </span>
+        <span>
+          <p>
+            Resume
+          </p>
+        </span>
+      </div>
+    )
+  }
+}
+
+export default Navbar
