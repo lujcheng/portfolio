e859ee23d8d931696bc7054d8dc8ef867462355c
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-03 20:57:56 -0400

    added projects and links to projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,22 @@ class Skills extends Component {
   render () {
     return (
       <div>
-        these are my projects
+        these are my projects:
+        <p>
+          My most recent project is a social media page built on microservices architecture and a MERN tech stack linked <a href='https://github.com/Renkinjutsu/Fudigram'>here</a>. 
+          <br></br>
+
+          I've also built a UI for SQL data in Entity-Relationship-Diagram format that can be queried hosted <a href='http://myschemaql.netlify.com'>here</a> and linked <a href='https://github.com/Renkinjutsu/My-Schema-QL'>here</a>. 
+          <br></br>
+
+          This is an e-commerce app built on Ruby on Rails linked <a href='https://github.com/Renkinjutsu/jungle-rails'>here</a>. 
+          <br></br>
+          
+          This is a chatting app built with websockets linked 
+          <a href='https://github.com/Renkinjutsu/Chatty-App'>here</a>.
+          <br></br>
+          
+        </p>
       </div>
     )
   }
