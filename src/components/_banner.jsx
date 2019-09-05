2ff56360d44fd10128f9fa27303acce31dfab8b4
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-04 23:57:35 -0400

    changed flexbox to bootstrap grid

--- src/components/_banner.jsx
+++ src/components/_banner.jsx
@@ -5,27 +5,29 @@ import TypistLoop from 'react-typist-loop'
 class Banner extends Component {
   render () {
     return (
-      <div id='banner'> 
-        <img src='./ebrastij.svg' />
-        <span>
-          <h3 id='name'>Lucas Cheng</h3>
-          <span> 
-            <p>I am a: </p>
-            <TypistLoop interval={1000}>
-              
-              {[
-                'Problem Solver',
-                'Developer',
-                'Good Person',
-              ].map(text =>
-                <Typist  avgTypingSpeed={40} key={text} startDelay={1000}>{text}
-                <Typist.Delay ms={1000} />
-                <Typist.Backspace count={text.length}/>
-                </Typist>
-              )}
-            </TypistLoop>
+      <div id='banner' className='container'> 
+        <div className='row'>
+          <img src='./ebrastij.svg' className='col-md-6 col-sm-12'/>
+          <span className='col-md-6 col-sm-12'>
+            <h3 id='name'>Lucas Cheng</h3>
+            <span> 
+              <p>I am a: </p>
+              <TypistLoop interval={1000}>
+                
+                {[
+                  'Problem Solver',
+                  'Developer',
+                  'Good Person',
+                ].map(text =>
+                  <Typist  avgTypingSpeed={40} key={text} startDelay={1000}>{text}
+                  <Typist.Delay ms={1000} />
+                  <Typist.Backspace count={text.length}/>
+                  </Typist>
+                )}
+              </TypistLoop>
+            </span>
           </span>
-        </span>
+        </div>
       </div>
     )
   }
