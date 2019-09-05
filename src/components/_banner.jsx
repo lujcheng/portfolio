8d80978289654a80eb61b49b7863b42c13237d76
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-04 20:59:08 -0400

    changed flex direction

--- src/components/_banner.jsx
+++ src/components/_banner.jsx
@@ -6,17 +6,20 @@ class Banner extends Component {
   render () {
     return (
       <div id='banner'> 
-        <img src='./ebrastij.svg' width='500px' height='500px' />
+        <img src='./ebrastij.svg' />
         <span>
           <p id='name'>Lucas Cheng</p>
-          I am a: 
-          <TypistLoop interval={3000}>
-            {[
-              'Problem Solver',
-              'Developer',
-              'Good Person',
-            ].map(text => <Typist key={text} startDelay={1000}>{text}</Typist>)}
-          </TypistLoop>
+          <span> 
+            <p>I am a: </p>
+            <TypistLoop interval={3000}>
+              
+              {[
+                'Problem Solver',
+                'Developer',
+                'Good Person',
+              ].map(text => <Typist key={text} startDelay={1000}>{text}</Typist>)}
+            </TypistLoop>
+          </span>
         </span>
       </div>
     )
