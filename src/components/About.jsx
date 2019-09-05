0d77127106fa423fcb7d59331daf5e8a0d0b5d9a
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-05 00:29:49 -0400

    added layout to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
 class About extends Component {
   render () {
     return (
-      <div id='about'> 
-        <p>
+      <div id='about' className='container'> 
+        <p className='offset-md-4'>
           My story starts in another industry, helping people heal their bodies.
         </p>
         <img src='' alt='rehab animation' />
-        <p>
+        <p className='col-md-8'>
           I felt something was missing in my career and searched for something different.
         </p>
         <img src='' alt='black hole?' />
