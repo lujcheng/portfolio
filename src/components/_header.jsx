ea0d5a7a1d2dee4d12436062b9c9bba5a712b9ab
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-05 01:15:12 -0400

    added drop down menu

--- src/components/_header.jsx
+++ src/components/_header.jsx
@@ -3,9 +3,16 @@ import React, { Component } from 'react'
 class Header extends Component {
   render () {
     return (
-      <span> 
-        <span>Contact</span>
-      </span>
+      <div className="btn-group dropleft">
+        <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          hamburger
+        </button>
+        <div className="dropdown-menu">
+          <a className='dropdown-item' href='#'>About Me</a>
+          <a className='dropdown-item' href='#'>Projects/Skills</a>
+          <a className='dropdown-item' href='#'>Contact Me</a>
+        </div>
+    </div>
     )
   }
 }
