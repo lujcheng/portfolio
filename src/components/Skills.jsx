bf46537f2e132d8c621c75521aac389ea8490fd9
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-05 00:54:12 -0400

    skill icons layout

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,35 +5,35 @@ class Skills extends Component {
       <div className='container'> 
         <div className='row justify-content-around'>
           <h3 className='col-12'>Web Front End</h3>
-          <img className='col-1 icon' src='./react-original.svg'  alt='react' />
-          <img className='col-1 icon' src='./jquery-original.svg'  alt='jquery' />
-          <img className='col-1 icon' src='./sass-original.svg'  alt='sass' /> 
+          <img className='col-4 icon' src='./react-original.svg'  alt='react' />
+          <img className='col-4 icon' src='./jquery-original.svg'  alt='jquery' />
+          <img className='col-4 icon' src='./sass-original.svg'  alt='sass' /> 
         </div>
         <div className='row justify-content-around'>
-          <h3 className='col-1 icon2'>Web Back End</h3>
-          <img className='col-1 icon' src='./nodejs-original.svg'  alt='node' />
-          <img className='col-1 icon' src='./rails-original-wordmark.svg'  alt='rails' />
-          <img className='col-1 icon' src='./express-original.svg'  alt='express' />
-          <img className='col-1 icon' src='./socket-io.svg'  alt='socket-io'  />
-          <img className='col-1 icon' src='./docker-original.svg'  alt='docker' /> 
-          <img className='col-1 icon' src='./mocha-plain.svg'  alt='mocha' />
-          <img className='col-1 icon' src='./babel-original.svg'  alt='babel' /> 
+          <h3 className='col-12'>Web Back End</h3>
+          <img className='col-4 icon' src='./nodejs-original.svg'  alt='node' />
+          <img className='col-4 icon' src='./rails-original-wordmark.svg'  alt='rails' />
+          <img className='col-4 icon' src='./express-original.svg'  alt='express' />
+          <img className='col-4 icon' src='./docker-original.svg'  alt='docker' /> 
+          <img className='col-4 icon' src='./mocha-plain.svg'  alt='mocha' />
+          <img className='col-4 icon' src='./socket-io.svg'  alt='socket-io'/>
+          <img className='col-4 icon' src='./babel-original.svg'  alt='babel' /> 
         </div>
         <div className='row justify-content-around'>
-          <h3 className='col-1 icon2'>Databases</h3>
-          <img className='col-1 icon' src='./postgresql-original.svg'  alt='psql' />
-          <img className='col-1 icon' src='./mongodb-original.svg'  alt='mongodb' />
+          <h3 className='col-12'>Databases</h3>
+          <img className='col-4 icon' src='./postgresql-original.svg'  alt='psql' />
+          <img className='col-4 icon' src='./mongodb-original.svg'  alt='mongodb' />
         </div>
         <div className='row justify-content-around'>
-          <h3 className='col-1 icon2'>Languages</h3>
-          <img className='col-1 icon' src='./javascript-original.svg'  alt='javascript' />
-          <img className='col-1 icon' src='./html5-original.svg'  alt= 'html5' />
-          <img className='col-1 icon' src='./css3-original.svg'  alt='CSS' />
+          <h3 className='col-12'>Languages</h3>
+          <img className='col-4 icon' src='./javascript-original.svg'  alt='javascript' />
+          <img className='col-4 icon' src='./html5-original.svg'  alt= 'html5' />
+          <img className='col-4 icon' src='./css3-original.svg'  alt='CSS' />
         </div>
         <div className='row justify-content-around'>
-          <h3 className='col-1 icon2'>Source Control Management</h3>
-          <img className='col-1 icon' src='./git-original.svg'  alt='git' />
-          <img className='col-1 icon' src='./github-original.svg'  alt='github' />
+          <h3 className='col-12'>Source Control Management</h3>
+          <img className='col-4 icon' src='./git-original.svg'  alt='git' />
+          <img className='col-4 icon' src='./github-original.svg'  alt='github' />
         </div>
       </div>
     )
