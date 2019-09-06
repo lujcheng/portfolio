04fa2d660ce9a9c98ee105d14ef744270e4b5d4b
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-06 17:20:04 -0400

    changing fonts and alignment for page, vh

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 class Skills extends Component {
   render () {
     return (
-      <div>
+      <div id='projects'>
         these are my projects:
         <p>
           My most recent project is a social media page built on microservices architecture and a MERN tech stack linked <a href='https://github.com/Renkinjutsu/Fudigram'>here</a>. 
@@ -17,6 +17,7 @@ class Skills extends Component {
           This is a chatting app built with websockets linked 
           <a href='https://github.com/Renkinjutsu/Chatty-App'>here</a>.
           <br></br>
+          mLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam quis enim lobortis scelerisque fermentum dui. Diam in arcu cursus euismod quis viverra nibh. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Ut sem viverra aliquet eget sit. Et ultrices neque ornare aenean euismod elementum nisi quis eleifend. Non tellus orci ac auctor augue mauris. Ut sem viverra aliquet eget sit amet tellus cras adipiscing. Euismod nisi porta lorem mollis aliquam ut porttitor leo a. Pellentesque massa placerat duis ultricies lacus. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. Vivamus at augue eget arcu dictum. Semper eget duis at tellus at urna condimentum mattis. Mattis nunc sed blandit libero volutpat. Tortor at auctor urna nunc id cursus metus aliquam. Donec et odio pellentesque diam. Egestas quis ipsum suspendisse ultrices gravida dictum. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Sed odio morbi quis commodo odio aenean sed adipiscing diam. Nunc sed augue lacus viverra vitae congue eu consequat. Interdum varius sit amet mattis vulputate enim nulla. Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae. Netus et malesuada fames ac turpis egestas integer.
           
         </p>
       </div>
