b9da5bfecb96445f3518510819d1b88b89e68ad0
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-09 17:41:58 -0400

    adjusted contact icon sizes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,14 +6,14 @@ class Contact extends Component {
     return (
       <div id='contact' className='text' >
         <p style={bold}>If you would like to collaborate, you're very welcome to contact me. </p>
-        <p>I'm available for employment</p>
+        <p>I'm available for employment.</p>
         <h5 style={bold}>Lucas Cheng</h5>
         <p className='col-12'>
           Full-stack Developer
           <br></br>
-          <i class="fab fa-github"></i>
-          <i class="fab fa-linkedin"></i>
-          <i class="fas fa-file"></i>
+          <i class="fab fa-github fa-2x"></i>
+          <i class="fab fa-linkedin fa-2x"></i>
+          <i class="fas fa-file fa-2x"></i>
         </p>
       </div>
     )
