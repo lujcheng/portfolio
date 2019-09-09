d9e40c5482dec3c8e8130a4f1666a2b547f330c3
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-09 17:32:54 -0400

    inline styling for contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
+const bold = { fontWeight: 'bold' }
 class Contact extends Component {
   render () {
     return (
       <div id='contact' className='text' >
-        <p>If you would like to collaborate, you're very welcome to contact me. </p>
+        <p style={bold}>If you would like to collaborate, you're very welcome to contact me. </p>
         <p>I'm available for employment</p>
-        <h5>Lucas Cheng</h5>
+        <h5 style={bold}>Lucas Cheng</h5>
         <p className='col-12'>
           Full-stack Developer
           <br></br>
