14b64c3a0dd562d2a947a64caeb2e0db8b8a3b24
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-09 02:16:37 -0400

    adjust menu alignemnet

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Contact extends Component {
   render () {
     return (
-      <div id='contact' className='container row text' > 
+      <div id='contact' className='text' > 
         <p className='col-12'>
           mLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam quis enim lobortis scelerisque fermentum dui. Diam in arcu cursus euismod quis viverra nibh. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Ut sem viverra aliquet eget sit. Et ultrices neque ornare aenean euismod elementum nisi quis eleifend. Non tellus orci ac auctor augue mauris. Ut sem viverra aliquet eget sit amet tellus cras adipiscing. Euismod nisi porta lorem mollis aliquam ut porttitor leo a. Pellentesque massa placerat duis ultricies lacus.
         </p>
