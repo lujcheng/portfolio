e74d08aa5a3603d7486e8750d6cee38ba3badaff
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-09 19:44:34 -0400

    added about me text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,19 +5,23 @@ class About extends Component {
     return (
       <div id='about' className='text container col-xl-7 col-lg-7 col-md-12 col-sm-12 col-xs-12' > 
         <a name='about'></a>
+       
         <p className='col-12'>
-          My story starts in another industry, helping people heal their bodies.
+          I've always like to break things down and put them back together again. 
         </p>
-        <img src='' alt='rehab animation' />
-        <p className='col-12'>
-          I felt something was missing in my career and searched for something different.
+        <p  className='col-12'>
+          It started at an early age with whatever I could get my hands on.
+          <br></br> 
+          Sometimes beds turned into forts, sometimes toys turned into little Frankensteinian monsters, sometimes newspapers turned into tanks.
+          <br></br>
         </p>
-        <img src='' alt='black hole?' />
-        <p className='col-12'>
-          Front End Web <br></br>
-          mLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam quis enim lobortis scelerisque fermentum dui. Diam in arcu cursus euismod quis viverra nibh. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Ut sem viverra aliquet eget sit. Et ultrices neque ornare aenean euismod elementum nisi quis eleifend. Non tellus orci ac auctor augue mauris. Ut sem viverra aliquet eget sit amet tellus cras adipiscing. Euismod nisi porta lorem mollis aliquam ut porttitor leo a. Pellentesque massa placerat duis ultricies lacus.
-
-Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. Vivamus at augue eget arcu dictum. Semper eget duis at tellus at urna condimentum mattis. Mattis nunc sed blandit libero volutpat. Tortor at auctor urna nunc id cursus metus aliquam. Donec et odio pellentesque diam. Egestas quis ipsum suspendisse ultrices gravida dictum. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Sed odio morbi quis commodo odio aenean sed adipiscing diam. Nunc sed augue lacus viverra vitae congue eu consequat. Interdum varius sit amet mattis vulputate enim nulla. Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae. Netus et malesuada fames ac turpis egestas integer.
+        <p  className='col-12'>
+          As I got older, I started breaking things down in other ways.
+          <br></br>
+          I developed an interest in sports and the human body, and that lead me to a career in Physical Therapy. 
+        </p>
+        <p  className='col-12'>
+          One day I started to try some online coding exercises, and it was everything I love to do, while be accessible and powerful at the same time. I enrolled and graduated from a coding bootcamp and so begins my programming career...
         </p>
       </div>
     )
