8c977aae50d20307d0ac9ce4adaca435a0caf16b
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-10 17:26:08 -0400

    added react fit text to about me and projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from 'react'
+import ReactFitText from 'react-fittext'
 class Skills extends Component {
   render () {
     return (
       <div id='projects'>
-        these are my projects:
-        <p>
-          My most recent project is a social media page built on microservices architecture and a MERN tech stack linked <a href='https://github.com/Renkinjutsu/Fudigram'>here</a>. 
-          <br></br>
+        <ReactFitText>
+          <p>
+            My most recent project is a social media page built on microservices architecture and a MERN tech stack linked <a href='https://github.com/Renkinjutsu/Fudigram'>here</a>. 
+            <br></br>
 
-          I've also built a UI for SQL data in Entity-Relationship-Diagram format that can be queried hosted <a href='http://myschemaql.netlify.com'>here</a> and linked <a href='https://github.com/Renkinjutsu/My-Schema-QL'>here</a>. 
-          <br></br>
+            I've also built a UI for SQL data in Entity-Relationship-Diagram format that can be queried hosted <a href='http://myschemaql.netlify.com'>here</a> and linked <a href='https://github.com/Renkinjutsu/My-Schema-QL'>here</a>. 
+            <br></br>
 
-          This is an e-commerce app built on Ruby on Rails linked <a href='https://github.com/Renkinjutsu/jungle-rails'>here</a>. 
-          <br></br>
-          
-          This is a chatting app built with websockets linked 
-          <a href='https://github.com/Renkinjutsu/Chatty-App'>here</a>.
-          <br></br>
-          mLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam quis enim lobortis scelerisque fermentum dui. Diam in arcu cursus euismod quis viverra nibh. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Ut sem viverra aliquet eget sit. Et ultrices neque ornare aenean euismod elementum nisi quis eleifend. Non tellus orci ac auctor augue mauris. Ut sem viverra aliquet eget sit amet tellus cras adipiscing. Euismod nisi porta lorem mollis aliquam ut porttitor leo a. Pellentesque massa placerat duis ultricies lacus. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. Vivamus at augue eget arcu dictum. Semper eget duis at tellus at urna condimentum mattis. Mattis nunc sed blandit libero volutpat. Tortor at auctor urna nunc id cursus metus aliquam. Donec et odio pellentesque diam. Egestas quis ipsum suspendisse ultrices gravida dictum. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Sed odio morbi quis commodo odio aenean sed adipiscing diam. Nunc sed augue lacus viverra vitae congue eu consequat. Interdum varius sit amet mattis vulputate enim nulla. Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae. Netus et malesuada fames ac turpis egestas integer.
-          
-        </p>
+            This is an e-commerce app built on Ruby on Rails linked <a href='https://github.com/Renkinjutsu/jungle-rails'>here</a>. 
+            <br></br>
+            
+            This is a chatting app built with websockets linked 
+            <a href='https://github.com/Renkinjutsu/Chatty-App'>here</a>.
+            <br></br>
+            
+          </p>
+
+        </ReactFitText>
       </div>
     )
   }
